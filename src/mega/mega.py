ff39501a71888e79cd16e104885b45c467bfb3e3
BRO <64780226+TG-BRO@users.noreply.github.com>
2024-08-09 15:17:27 +0530

    Update mega.py

--- src/mega/mega.py
+++ src/mega/mega.py
@@ -750,9 +750,9 @@ class Mega:
                 # Edit status message
                 percentage = file_info.st_size * 100 / file_size
                 
-                progress = "┏━━━━✦[{0}{1}]✦━━━━".format(
-                  ''.join(["▣" for i in range(math.floor(percentage / 10))]),
-                  ''.join(["▢" for i in range(10 - math.floor(percentage / 10))])
+                progress = "`[{0}{1}]` \n".format(
+                  ''.join(["▣" for i in range(math.floor(percentage / 5))]),
+                  ''.join(["▢" for i in range(10 - math.floor(percentage / 5))])
                 )
 
                 ok = "`{0}%`".format(
@@ -760,7 +760,7 @@ class Mega:
                 )
 
                 try:
-                  dlstats_msg.edit(f"📥 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆 📥\n{progress}\n┣📦 Pʀᴏɢʀᴇꜱꜱ : {ok}\n┣ 📛 Nᴀᴍᴇ : {file_name}\n┣ ✅ Dᴏɴᴇ : {humanize.naturalsize(file_info.st_size)}\n┣ 📁 Tᴏᴛᴀʟ : {humanize.naturalsize(file_size)}\n┗━━━━━━━━━━━━━━━━━━━━", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⛔ Cancel", callback_data="cancel_mega")]]))
+                  dlstats_msg.edit(f"**📥Downloading Mega Link...**\n{progress}\n➩ 📦**Progress**: {ok} \n➩ 📛**Name**: `{file_name}` \n➩ ✅**Done**: `{humanize.naturalsize(file_info.st_size)}` \n➩ 📁**Size**: `{humanize.naturalsize(file_size)}`", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("❌ Cancel", callback_data="cancel_mega")]]))
                   logger.info('%s of %s downloaded', file_info.st_size,
                             file_size)
                 except MessageNotModified:
